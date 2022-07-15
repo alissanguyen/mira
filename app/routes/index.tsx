aedce28f60b04599b36bd254951b0338324f73ff
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-07-15 13:19:12 -0700

    Fix deployment test failed due to mismatching in models, add sample data and design

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,24 @@
 export default function IndexPage() {
     return (
-        <div>
-            <h1 className="flex text-start m-auto text-5xl leading-tight font-bold text-gray-700 max-w-screen-md">Welcome to my learning space. <br></br>Follow if you want to tag along as I learn new things everyday ☻</h1>
+        <div className="flex flex-col max-w-screen-md m-auto">
+            <h1 className="flex text-start text-5xl leading-tight font-bold text-gray-700 mb-16">Welcome to my learning space. <br></br>Follow if you want to tag along as I learn new things everyday ☻</h1>
+            <ExampleNoteCard />
+        </div>
+    )
+}
+
+function ExampleNoteCard() {
+    return (
+        <div className="Note__Container rounded-xl bg-white flex flex-col align-start p-5">
+            <span className="Note__Title font-bold text-lg mb-2">Negative indexing with array.prototype.at()</span>
+            <div className="Note__Description flex flex-col">
+                <span>The .at() method takes an integer and return the item at that index, allows both negative and positive value. Negative indexes count back from the last item of the array.</span>
+                <ul className="list-disc">
+                    <li className="ml-10">For example we have an array <code>A = [1, 2, 5, 15]</code>. <code>A.at(-1)</code> will return 15.</li>
+                    <li className="ml-10"><code>array[array.length-1]</code> is equivalent to <code>array.at(-1)</code>.</li>
+                </ul>
+                <span className="italic text-gray-400 text-end">Friday Jul 15 2022 1:18 PM</span>
+            </div>
         </div>
     )
 }
